98e3eaaec23d3601334ad5613a05a12e86fa533c
meirdev <maornet@gmail.com>
2026-05-12 13:27:15 +0300

    use terser for the build

--- challenge-page/vite.config.ts
+++ challenge-page/vite.config.ts
@@ -6,4 +6,7 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
+  build: {
+    minify: "terser",
+  },
 });
